9b8916b90ea943a1d81ee3aeb69ab3f999083755
ernest <efran7107@gmail.com>
2023-12-02 15:16:40 -0700

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1>Fullmetal Alchemist</h1>
+      <nav>
+        <li>about us</li>
+        <li>info</li>
+        <li>support us</li>
+      </nav>
+    </header>
+  );
+};
